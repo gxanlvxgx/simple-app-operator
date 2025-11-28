dd1232c9f6befdf8e3c3fb73a7c434db6517b5f2
gxanlvxgx <gianluigi.romanooo@gmail.com>
2025-11-28 12:54:16 +0100

    Initial commit without workflows

---
+++ api/v1/simpleapp_types.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// SimpleAppSpec defines the desired state of SimpleApp
+type SimpleAppSpec struct {
+	// Image is the Docker image to run (e.g. nginx:latest, my-app:v1)
+	// +kubebuilder:validation:Required
+	Image string `json:"image"`
+
+	// Replicas defines how many instances of the application to run
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:default=1
+	Replicas int32 `json:"replicas,omitempty"`
+
+	// ContainerPort is the port the application listens on inside the container
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=65535
+	ContainerPort int32 `json:"containerPort"`
+
+	// ServicePort is the port exposed by the Kubernetes Service to the cluster
+	// +kubebuilder:default=80
+	ServicePort int32 `json:"servicePort,omitempty"`
+}
+
+// SimpleAppStatus defines the observed state of SimpleApp
+type SimpleAppStatus struct {
+	// ReadyReplicas tells us how many pods are actually running
+	ReadyReplicas int32 `json:"readyReplicas"`
+
+	// ServiceStatus reports the general health (e.g., "Created", "Pending")
+	ServiceStatus string `json:"serviceStatus,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Image",type="string",JSONPath=".spec.image"
+//+kubebuilder:printcolumn:name="Replicas",type="integer",JSONPath=".spec.replicas"
+//+kubebuilder:printcolumn:name="Ready",type="integer",JSONPath=".status.readyReplicas"
+//+kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+
+// SimpleApp is the Schema for the simpleapps API
+type SimpleApp struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SimpleAppSpec   `json:"spec,omitempty"`
+	Status SimpleAppStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SimpleAppList contains a list of SimpleApp
+type SimpleAppList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SimpleApp `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SimpleApp{}, &SimpleAppList{})
+}
